dedc829dadae5dc831880f73483bb8db4db75e3b
hepeng <hepeng126@foxmail.com>
2021-08-17 18:30:31 +0800

    地址

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App'
+// import VueJsonp from 'vue-jsonp'
+	
 import {myRequsest} from 'util/api.js'
 
 Vue.prototype.$myRequsest = myRequsest
+
+// Vue.use(VueJsonp)
+// const vm = new Vue
+
 Vue.filter('formatDate', function(value) {
 	value = value.slice(5)
 	return value
--- pages/contact/contact.vue
+++ pages/contact/contact.vue
@@ -3,7 +3,7 @@
 		<image src="http://www.itcast.cn/2018czydz/images/gywmban.jpg"></image>
 		<view class="info">
 			<view @click="phone">联系电话：123-456-7891(点击拨打)</view>
-			<view>地址：重庆市璧山区</view>
+			<view>地址：{{address}}</view>
 		</view>
 		<map class="map" :longitude="longitude" :latitude="latitude" :markers="markers" :scale="scale"></map>
 	</view>
@@ -12,38 +12,61 @@
 	export default {
 		data() {
 			return {
-				longitude:116.397128,
-				latitude:39.916527,
-				scale:15,
-				markers:[]
+				longitude: 116.397128,
+				latitude: 39.916527,
+				scale: 15,
+				address: '',
+				markers: []
 			}
 		},
 		methods: {
-			phone(){
+			phone() {
 				uni.makePhoneCall({
-					phoneNumber:'123-456-7891'
+					phoneNumber: '123-456-7891'
 				})
 			}
 		},
 		onLoad() {
 			uni.getLocation({
-			    type: 'wgs84',
-			    success:  (res)=> {
-					this.longitude=res.longitude
-					this.latitude=res.latitude
-					this.markers=[]
+				type: 'wgs84',
+				geocode: 'true',
+				success: (res) => {
+					if (res.address) {
+						this.address = res.address.province + res.address.city + res.address.district
+					} else {
+						uni.request({
+							url: 'https://restapi.amap.com/v3/ip?key=5993732db87ed6c48dd3606712410416',//高德ip定位api
+							// url:`http://apis.map.qq.com/ws/geocoder/v1`,
+							method:'get',
+							// data:{
+							// 	location:`${res.latitude},${res.longitude}`,
+							// 	key:'BMSBZ-Q24WX-IBF4E-T4PIY-KSXQZ-V2FFQ',
+							// },
+							success: res => {
+								console.log(res)
+								this.address = res.data.city
+							},
+							fail: err => {
+								console.log(err);
+							}
+						});
+					}
+					this.longitude = res.longitude
+					this.latitude = res.latitude
+					this.markers = []
 					this.markers.push({
-						longitude:res.longitude,
-						latitude:res.latitude,
-						iconPath:'../../static/tabber/home(1).png',
-						width:30,
-						height:30
+						longitude: res.longitude,
+						latitude: res.latitude,
+						iconPath: '../../static/tabber/home(1).png',
+						width: 30,
+						height: 30
 					})
-			    }
+				}
 			});
+
 		},
-		created(){
-			
+		created() {
+
 		}
 	}
 </script>
@@ -64,7 +87,8 @@
 				border-bottom: 1px solid #eee;
 			}
 		}
-		.map{
+
+		.map {
 			width: 750rpx;
 			height: 735rpx;
 		}
